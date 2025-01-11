dfb597f1e6a7bbf535f5b75440d2dd2b9473a56d
OwlbanGroup <142179795+OwlbanGroup@users.noreply.github.com>
2025-01-11 01:11:22 -0500

    Add Web3 integration and update requirements for Ethereum connectivity

--- model_setup.py
+++ model_setup.py
@@ -22,17 +22,3 @@ model = Sequential([
 
 # Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-# Dummy data
-X = np.array([[0, 1, 2], [1, 2, 3], [2, 3, 4]])
-y = np.array([0, 1, 0])
-
-# Train the model
-model.fit(X, y, epochs=10)
-
-# Make predictions
-predictions = model.predict(X)
-
-# Format and print predictions
-for i, prediction in enumerate(predictions):
-    print(f"Prediction for input {X[i]}: {prediction[0]}")
