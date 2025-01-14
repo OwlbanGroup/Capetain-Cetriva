3c382df1bbbd817ea057d7caa394308908c01cd6
OwlbanGroup <142179795+OwlbanGroup@users.noreply.github.com>
2025-01-14 00:43:07 -0500

    Add token value mapping and initialize token value in NFT contract

--- NFTContract.sol
+++ NFTContract.sol
@@ -7,6 +7,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract CapetainCetrivaNFT is ERC721, Ownable {
     uint256 public nextTokenId;
     string public baseTokenURI;
+    mapping(uint256 => uint256) public tokenValues;
+    uint256 public totalValue;
 
     constructor(string memory _baseTokenURI) ERC721("CapetainCetrivaNFT", "CCTNFT") {
         baseTokenURI = _baseTokenURI;
@@ -14,6 +16,8 @@ contract CapetainCetrivaNFT is ERC721, Ownable {
 
     function mint(address to) external onlyOwner {
         _safeMint(to, nextTokenId);
+        tokenValues[nextTokenId] = 250000 * 10**18; // Assuming 18 decimal places for the value
+        totalValue += 250000 * 10**18; // Update total value
         nextTokenId++;
     }
 
@@ -23,15 +27,11 @@ contract CapetainCetrivaNFT is ERC721, Ownable {
 
     function mintCoetusAppNFT(address fundAddress) external onlyOwner {
         _safeMint(fundAddress, nextTokenId);
+        tokenValues[nextTokenId] = 250000 * 10**18;
+        totalValue += 250000 * 10**18; // Update total value
         nextTokenId++;
     }
 
     function mintOwlbanGroupNFT(address fundAddress) external onlyOwner {
         _safeMint(fundAddress, nextTokenId);
-        nextTokenId++;
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseTokenURI;
-    }
 }
