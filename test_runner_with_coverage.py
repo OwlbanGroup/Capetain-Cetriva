88a2d1724b9fc2fc64cc8a48750cac8d7a5581ff
Sean Broome <bsean4890@gmail.com>
2025-06-17 03:34:05 -0400

    UPDATE

--- test_runner_with_coverage.py
+++ test_runner_with_coverage.py
@@ -1,5 +1,6 @@
 import coverage
 import unittest
+import sys
 
 
 def run_tests_with_coverage():
@@ -8,9 +9,9 @@ def run_tests_with_coverage():
 
     # Discover and run all tests
     loader = unittest.TestLoader()
-    tests = loader.discover('.')
+    tests = loader.discover('.', pattern='test_*.py')
     testRunner = unittest.TextTestRunner(verbosity=2)
-    _ = testRunner.run(tests)
+    result = testRunner.run(tests)
 
     cov.stop()
     cov.save()
@@ -23,6 +24,14 @@ def run_tests_with_coverage():
     print(
         "HTML version of coverage report generated in coverage_html_report directory."
     )
+
+    # Exit with appropriate status code
+    if not result.wasSuccessful():
+        print("Some tests failed.")
+        sys.exit(1)
+    else:
+        print("All tests passed successfully.")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
